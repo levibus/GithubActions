375c1d82eb8cea6b0dbd155f0780434786b7524d
Levi Busching <levibusching@gmail.com>
2024-04-10 22:01:25 -0500

    right unit test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -109,7 +109,7 @@ namespace GithubActionsLab
         {
             Assert.AreEqual(1, Program.Power("1", "2"));
             Assert.AreEqual(9, Program.Power("3", "2"));
-            Assert.AreEqual(125, Program.Add("5", "3"));  // wrong
+            Assert.AreEqual(125, Program.Power("5", "3")); 
         }
 
         [Test]
